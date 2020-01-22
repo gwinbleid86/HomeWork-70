332a782b6b7edd64f81ba39908f624a432bffa2b
Podstavin Aleksei <gwinbleid86@gmail.com>
2020-01-23 02:34:40 +0600

    add Home.js

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Enter name of movie into the search field</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
